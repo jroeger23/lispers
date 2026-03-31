aa0ba6ed7a014a6b97a4ab9ff30fde7432f18efb
Jonas Röger <jonas.roeger@tu-dortmund.de>
2026-04-01 00:37:25 +0200

    feat(scenes): add materials.lisp

---
+++ scenes/materials.lisp
@@ -0,0 +1,37 @@
+(set 'red
+     (material
+      (color 1 0 0)
+      (color 1 0 0)
+      (color 0.5 0 0)
+      50 0.25))
+(set 'blue
+     (material
+      (color 0 0 1)
+      (color 0 0 1)
+      (color 0 0 0.6)
+      50 0.25))
+(set 'green
+     (material
+      (color 0 1 0)
+      (color 0 1 0)
+      (color 0 0.6 0)
+      50 0.25))
+(set 'white
+     (material
+      (color 1 1 1)
+      (color 1 1 1)
+      (color 0.6 0.6 0.6)
+      100 0.5))
+(set 'black
+     (material
+      (color 0 0 0)
+      (color 0 0 0)
+      (color 0.6 0.6 0.6)
+      100 0.5))
+
+(set 'dark-mirror
+     (material
+      (color 0 0 0)
+      (color 0 0 0)
+      (color 0.2 0.2 0.2)
+      20 0.6))
